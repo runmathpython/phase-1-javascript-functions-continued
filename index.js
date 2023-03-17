e9dd1bd49684a19db7a8a7e81b87cdaa2365db7b
Seong Kim <runmath@gmail.com>
2023-03-16 18:14:00 -0600

    complete Functions: Continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(highlight = "*") {
+    return function (theAdj) {
+        return `You are ${highlight}${theAdj}${highlight}!`;
+        };
+}
